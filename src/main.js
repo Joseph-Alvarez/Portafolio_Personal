06d81da19a2a275ec278843dce9b45512fa84220
Joseph Alvarez <phordonez42@gmail.com>
2025-03-23 14:52:11 -0600

    Nueva Actualizacion_5

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from '@/components/App.vue';
 // Importa los estilos
 import '../styles/main.css';
 import '../styles/style.css';
-import '../src/particles.min.js';
+import '../src/js/particles.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: './',
+  // Change from relative to absolute path
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  server: {
-    port: 3000,
-    open: true,
-  },
   build: {
-    outDir: 'dist', // Carpeta donde se guardará el build final
+    outDir: 'dist',
     emptyOutDir: true,
   },
 });
