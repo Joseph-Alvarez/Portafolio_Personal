af714dadf5056b064d13e07889dbf2a5b26ba41a
Joseph Alvarez <phordonez42@gmail.com>
2025-03-23 14:32:34 -0600

    Nueva Actualizacion_4

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue';
 import App from '@/components/App.vue';
 
 // Importa los estilos
-import '/styles/main.css';
-import '/styles/style.css';
+import '../styles/main.css';
+import '../styles/style.css';
+import '../src/particles.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).mount('#app');
