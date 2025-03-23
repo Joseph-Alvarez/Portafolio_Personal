c3a243346e324a179df44fe1c3ab31e45aab82cf
Joseph Alvarez <phordonez42@gmail.com>
2025-03-23 00:28:13 -0600

    primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './components/layout';
+import Controlador from './components/controlador';
+
+function App() {
+  return (
+    <Layout>
+      <Controlador />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import App from '@/components/App.vue';
+
+// Importa los estilos
+import '/styles/main.css';
+import '/styles/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+createApp(App).mount('#app');
